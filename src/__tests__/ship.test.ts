0d760c3de4f16b9eadd0d850a265c0e2fd26fe3a
Brady <brady.savarie@hotmail.com>
2023-05-11 23:15:43 -0400

    test and write isSunk method

--- src/__tests__/ship.test.ts
+++ src/__tests__/ship.test.ts
@@ -7,3 +7,12 @@ describe('test hits method', () => {
         expect(battleship.hits).toBe(1);
     });
 });
+
+describe('check if ship is sunk', () => {
+    it('Sinks ship and tests isSunk for accuracy', () => {
+        let patrolBoat = new Ship(2);
+        patrolBoat.hit();
+        patrolBoat.hit();
+        expect(patrolBoat.isSunk()).toBe(true);
+    });
+});
--- src/models/ship.ts
+++ src/models/ship.ts
@@ -1,17 +1,21 @@
-/* Represents a single ship on the board, with properties like its length, number of hits, and isSunk */
-
 export class Ship {
     length: number;
     hits: number;
-    isSunk: boolean;
 
     constructor(length: number) {
         this.length = length;
         this.hits = 0;
-        this.isSunk = false;
     }
 
     hit() {
         this.hits++;
+    }
+
+    isSunk() {
+        if (this.hits >= this.length) {
+            return true;
+        } else {
+            return false;
+        }
     }
 }
