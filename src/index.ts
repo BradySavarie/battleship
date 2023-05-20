d85d8c6b8efb4a4df05c421429a92ecdf0d5cd27
Brady <brady.savarie@hotmail.com>
2023-05-19 20:20:21 -0400

    Place computers ships and write active player functions

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-import { Human } from './models/player';
+import { Human, Computer } from './models/player';
+
+let human = new Human();
+let computer = new Computer();
+placeComputerShips(computer);
+
+function placeComputerShips(computer: Computer) {
+    // for each ship
+    computer.ships.forEach(function (value, index) {
+        let isSuccessful;
+        // while the ship has not successfully been placed on the board
+        while (!isSuccessful) {
+            // randomly generate a valid coordinate pair
+            let row = Math.floor(Math.random() * 10);
+            let col = Math.floor(Math.random() * 10);
+            // attempt to place the ship on the board
+            isSuccessful = computer.board.placeShip(value, index, [row, col]);
+        }
+    });
+}
--- src/models/game.ts
+++ src/models/game.ts
@@ -1,1 +1,12 @@
 /* Manages the game state, including player turns, game status (ongoing, victory, defeat), and other game-related data. */
+import { Human, Computer } from './player';
+
+let activePlayer: Human | Computer;
+
+function setActivePlayer(player: Human | Computer) {
+    activePlayer = player;
+}
+
+function getActivePlayer(): Human | Computer {
+    return activePlayer;
+}
